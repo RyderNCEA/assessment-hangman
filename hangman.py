09cc3e3a5ce7cbaf793c03622c13ec3426ce1c8b
Ryder G <55562141+RyderNCEA@users.noreply.github.com>
2021-03-17 14:19:17 +1300

    Add Dictionary Function

--- hangman.py
+++ hangman.py
@@ -1,1 +1,10 @@
+import random
 
+# Load list of words from word_list.txt file
+def get_dictionary():
+    with open("word_list.txt", "r") as words:
+        dictionary = []
+        lines = words.readlines()
+        for line in lines:
+            dictionary.append(line.replace("\n", ""))
+        return dictionary

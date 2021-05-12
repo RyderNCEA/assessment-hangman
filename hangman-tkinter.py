bd9988da9741d6e8f68226e0bcd3fa691d9edd3d
Ryder G <55562141+RyderNCEA@users.noreply.github.com>
2021-05-12 18:18:15 +1200

    Start creation of Hangman GUI
    Add creation of the window.

---
+++ hangman-tkinter.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+import random
+
+window = tk.Tk()
+
+window.title("Hangman Game")
+window.geometry('x'.join(WINDOW_DIMENSIONS))
+window.configure(bg="#F9EBD1")
+
+window.mainloop()
